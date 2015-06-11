2acd0ef71fb8d0b6fec4f51911eab2e3912989b3
seairy <seairy@gmail.com>
2015-06-11 09:21:11 +0800

    init commit

--- app/uploaders/ckeditor_picture_uploader.rb
+++ app/uploaders/ckeditor_picture_uploader.rb
@@ -3,11 +3,26 @@ class CkeditorPictureUploader < BaseUploader
   include Ckeditor::Backend::CarrierWave
   process :read_dimensions
 
-  version :thumb do
-    process :resize_to_fill => [118, 100]
-  end
-
   version :content do
     process :resize_to_limit => [1800, 1350]
+  end
+
+  version :thumb do
+    process :resize_to_fit => [160, 120]
+  end
+
+  version :w1800_h1350_ft_q80 do
+    process quality: 80
+    process resize_to_fit: [1800, 1350]
+  end
+
+  version :w800_h600_ft_q80 do
+    process quality: 80
+    process resize_to_fit: [800, 600]
+  end
+
+  version :w640_h480_ft_q80 do
+    process quality: 80
+    process resize_to_fit: [640, 480]
   end
 end
