8eb66d07d67f36752a7266f4dac37c1960819d32
seairy <seairy@gmail.com>
2015-06-11 09:56:26 +0800

    init commit

--- app/controllers/frontend/home_controller.rb
+++ app/controllers/frontend/home_controller.rb
@@ -2,5 +2,7 @@
 class Frontend::HomeController < Frontend::BaseController
   def index
     @posts = Post.published.order(published_at: :desc)
+    @gallary_posts = Post.published.sample(8)
+    @archive_posts = Post.published.order(:published_at).limit(24)
   end
 end
