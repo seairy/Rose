7028a553e8063f345c7f0d58c47e4181b6ad8b71
seairy <seairy@gmail.com>
2015-06-16 22:59:32 +0800

    init commit

--- app/controllers/frontend/home_controller.rb
+++ app/controllers/frontend/home_controller.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 class Frontend::HomeController < Frontend::BaseController
   def index
-    @posts = Post.published.order(published_at: :desc)
+    @posts = Post.published.order(published_at: :desc).page(params[:page]).per(25)
     @carousel_posts = Post.published.displayed_in_carousel.order(published_at: :desc)
     @recommended_posts = Post.published.displayed_in_recommended.order(published_at: :desc)
     @gallary_posts = Post.published.sample(8)
