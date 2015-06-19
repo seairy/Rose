c54e179c7e9b0acaaa3eedafc57e9bf10ab79aac
seairy <seairy@gmail.com>
2015-06-19 15:38:27 +0800

    init commit

--- app/controllers/frontend/categories_controller.rb
+++ app/controllers/frontend/categories_controller.rb
@@ -2,8 +2,10 @@
 class Frontend::CategoriesController < Frontend::BaseController
   def show
     @category = Category.find(params[:id])
-    @posts = @category.posts.published
+    @posts = @category.posts.published.page(params[:page]).per(25)
+    @popular_posts = @category.posts.published.order(page_view: :desc).limit(24)
     @carousel_posts = @category.posts.published.sample(8)
+    @recommended_posts = Post.published.displayed_in_recommended.order(published_at: :desc)
     @gallary_posts = Post.published.sample(8)
     @archive_posts = Post.published.order(:published_at).limit(24)
   end
