53739c4a95c42251471590f69f3d9d308ed5593d
seairy <seairy@gmail.com>
2015-08-10 12:18:57 +0800

    init commit

--- app/uploaders/magazine_cover_uploader.rb
+++ app/uploaders/magazine_cover_uploader.rb
@@ -1,12 +1,17 @@
 # encoding: utf-8
 class MagazineCoverUploader < BaseUploader
-  version :w800_h600_fl_q80 do
+  version :w1300_h1850_fl_q80 do
     process quality: 80
-    process resize_to_fill: [800, 600]
+    process resize_to_fill: [1300, 1850]
   end
 
-  version :w160_h120_fl_q80 do
+  version :w600_h800_fl_q80 do
     process quality: 80
-    process resize_to_fill: [160, 120]
+    process resize_to_fill: [600, 800]
+  end
+
+  version :w300_h400_fl_q80 do
+    process quality: 80
+    process resize_to_fill: [300, 400]
   end
 end
