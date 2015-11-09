d5c6d54d5d772818b9dfe7ae8bdebe0f52363713
seairy <seairy@gmail.com>
2015-11-09 08:55:13 +0800

    init commit

--- app/uploaders/expert_portrait_uploader.rb
+++ app/uploaders/expert_portrait_uploader.rb
@@ -4,4 +4,9 @@ class ExpertPortraitUploader < BaseUploader
     process quality: 80
     process resize_to_fill: [340, 340]
   end
+
+  version :w255_h340_fl_q80 do
+    process quality: 80
+    process resize_to_fill: [255, 340]
+  end
 end
